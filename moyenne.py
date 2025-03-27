ce273586577d3b41caaa05694e8f895423d4f0d6
ihavesandineyes <fudempror@gmail.com>
2025-03-27 16:19:40 +0100

    changes

---
+++ moyenne.py
@@ -0,0 +1,2 @@
+def calcul_moyenne(liste):
+    return sum(liste) / len(liste) if liste else 0
---
+++ tests/test_moyenne.py
@@ -0,0 +1,7 @@
+import pytest
+from moyenne import calcul_moyenne
+
+def test_calcul_moyenne():
+    assert calcul_moyenne([1,2,3]) == 2
+    assert calcul_moyenne([10,20,30,40]) == 25
+    assert calcul_moyenne([]) == 0
