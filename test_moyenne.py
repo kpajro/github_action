6758369c350d22e57aa594df7a37b055b20eaaa7
ihavesandineyes <fudempror@gmail.com>
2025-03-27 16:40:50 +0100

    remove tests folder

--- test_moyenne.py
+++ test_moyenne.py
@@ -1,7 +1,6 @@
-import pytest
 from moyenne import calcul_moyenne
 
 def test_calcul_moyenne():
     assert calcul_moyenne([1,2,3]) == 2
-    assert calcul_moyenne([10,20,30,40]) == 25
+    assert calcul_moyenne([10,20,30,40]) == 24
     assert calcul_moyenne([]) == 0
